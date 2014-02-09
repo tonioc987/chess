5c6a8cfa1c706b0ba1cef9b4872b4144e86e4f48
Antonio <tonioc987@gmail.com>
2014-02-09 12:46:59 -0600

    Project just for code logic.

---
+++ game_logic_code/src/Board.h
@@ -0,0 +1,36 @@
+/*
+ *  Chess
+ *  Copyright (C) 2013  A. Cortes
+ *  This program is under the terms of the GNU GPL v3
+ *  See LICENSE file in the root of this project
+ */
+#ifndef BOARD_H_
+#define BOARD_H_
+
+#include <vector>
+#include "Common.h"
+
+namespace acortes {
+namespace chess {
+
+class Piece;
+
+class Board {
+public:
+  Board(uint8_t num_files, uint8_t num_ranks);
+  void PutPiece(Piece * piece, uint8_t file, uint8_t rank);
+  void PutPiece(Piece * piece, std::string move);
+  void RemovePiece(Piece * piece);
+
+protected:
+  uint8_t num_files_;
+  uint8_t num_ranks_;
+  std::vector<Piece *> pieces_;
+  std::vector<std::vector<Piece*> > board_;
+  Piece ** FindPiece(Piece * piece);
+};
+
+}
+}
+
+#endif /* BOARD_H_ */
