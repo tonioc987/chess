0c22d1f04205a2f0dbfe45ce7a570ff26c39cf24
Antonio <tonioc987@gmail.com>
2014-02-11 04:03:18 -0600

    Project just for testing.

---
+++ game_logic_test/test/AllTests.cpp
@@ -0,0 +1,12 @@
+/*
+ *  Chess
+ *  Copyright (C) 2013  A. Cortes
+ *  This program is under the terms of the GNU GPL v3
+ *  See LICENSE file in the root of this project
+ */
+#include "gtest/gtest.h"
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
