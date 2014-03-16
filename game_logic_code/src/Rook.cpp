ac7aa0f55e18486a7c41bd0e8ab711d40d39f68c
Antonio <tonioc987@gmail.com>
2014-03-16 10:31:48 -0500

    Verify no pieces in between for rook movements
    In general, there is no verification of empty squares for the rest of the pieces. Assuming, the pgn is valid, but for rooks there can be some ambiguity if there is no validation for empty squares.

--- game_logic_code/src/Rook.cpp
+++ game_logic_code/src/Rook.cpp
@@ -5,6 +5,7 @@
  */
 
 #include "Rook.h"
+#include "Board.h"
 #include <cassert>
 
 namespace acortes {
@@ -42,9 +43,25 @@ bool Rook::IsValidMove(int new_file, int new_rank) const {
     return false;
   } else if(file_movement == 0 && rank_movement !=0) {
     // forward/backward movement
+    // check no pieces in between
+    int start_rank = (rank_ < new_rank) ? rank_ + 1 : new_rank + 1;
+    int end_rank = (rank_ < new_rank) ? new_rank : rank_;
+    for(int r = start_rank; r < end_rank; r++) {
+      if(board_->GetPiece(file_, r) != nullptr) {
+        return false;
+      }
+    }
     return true;
   } else if(file_movement != 0 && rank_movement ==0) {
     // left/right movement
+    // check no pieces in between
+    int start_file = (file_ < new_file) ? file_ + 1 : new_file + 1;
+    int end_file = (file_ < new_file) ? new_file : file_;
+    for(int f = start_file; f < end_file; f++) {
+      if(board_->GetPiece(f, rank_) != nullptr) {
+        return false;
+      }
+    }
     return true;
   }
   return false;
